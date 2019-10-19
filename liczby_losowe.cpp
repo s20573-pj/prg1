00736ff00d7b3f1801657665e9a0d00d2fbddfba
Pawel <s20573@pjwstk.edu.pl>
2019-10-19 17:01:48 +0200

    usprawniono sortowanie liczby :)

--- liczby_losowe.cpp
+++ liczby_losowe.cpp
@@ -4,30 +4,31 @@
 
 void naive_sort(int const N, int* an_array)
 {
-    for (auto i = 0; i < N; ++i) {
-        int* the_smallest = &an_array[i];
+	for (auto i = 0; i < N; ++i)
+	{
+	        int* the_smallest = &an_array[i];
+			for (auto j = i; j < N; ++j)
+			{
+		            if (an_array[j] < *the_smallest)
+				{
+        		        the_smallest = &an_array[j];
+				}
+        		}
 
-        for (auto j = i; j < N; ++j) {
-            if (an_array[j] < *the_smallest) {
-                the_smallest = &an_array[j];
-            }
-        }
-
-        auto tmp = *the_smallest;
-
-        *the_smallest = an_array[i];
-
-        an_array[i] = tmp;
-    }
+        	auto tmp = *the_smallest;
+		*the_smallest = an_array[i];
+		an_array[i] = tmp;
+    	}
 }
 
 void random_initialisation(int const N, int* an_array)
 {
-    std::random_device rd;
-    std::uniform_int_distribution<int> generator(0, 100);
-    for (auto i = 0; i < N; ++i) {
-        an_array[i] = generator(rd);
-    }
+	std::random_device rd;
+	std::uniform_int_distribution<int> generator(0, 100);
+	for (auto i = 0; i < N; ++i)
+	{
+  	      an_array[i] = generator(rd);
+    	}
 }
 
 int main(int argc, char* argv[])
