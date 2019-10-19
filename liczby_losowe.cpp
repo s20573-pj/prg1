a9445ab5824067db2a60436d56378106e4734076
Pawel <s20573@pjwstk.edu.pl>
2019-10-19 16:10:52 +0200

    dodano sortowanie liczby :)

--- liczby_losowe.cpp
+++ liczby_losowe.cpp
@@ -2,24 +2,46 @@
 #include <iostream>
 #include <random>
 
-int main()
+void naive_sort(int const N, int an_array[])
 {
 
-    auto const N = 10;
+    for (auto i = 0; i < N; ++i) 
+	{
 
+        auto the_smallest = an_array[i];
+        auto index = i;
 
-    int an_array[N];
+        for (auto j = i; j < N; ++j)
+ 		{
+            if (an_array[j] < the_smallest) 
+			{
+                the_smallest = an_array[j];
+                index = j;
+           		 }
+       		 }
 
-    {
+        an_array[index] = an_array[i];
+        an_array[i] = the_smallest;
+    	}
+}
 
-        std::random_device rd;
-        std::uniform_int_distribution<int> generator(0, 100);
-        for (auto i = 0; i < N; ++i) {
-            an_array[i] = generator(rd);
-        }
+void random_initialisation(int const N, int an_array[])
+{
+    std::random_device rd;
+    std::uniform_int_distribution<int> generator(0, 100);
+    for (auto i = 0; i < N; ++i) {
+        an_array[i] = generator(rd);
     }
+}
 
-    std::cout << "sizeof(an_array): " << sizeof(an_array) << "\n";
+int main()
+{
+    auto const N = 10;
+    int an_array[N];
+    random_initialisation(N, an_array);
+
+    naive_sort(N, an_array);
+
     for (auto i = 0; i < N; ++i) {
         std::cout << an_array[i] << "\n";
     }
